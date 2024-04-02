abfbbcf6206411ff977bb40db82d887ce73155dd
ThienHuongZz <huongy138@gmail.com>
2024-04-03 00:06:20 +0700

    ex2

--- Lab2_NTTHuong/Core/Src/software_timer.c
+++ Lab2_NTTHuong/Core/Src/software_timer.c
@@ -5,61 +5,58 @@
  *      Author: glmun
  */
 #include "software_timer.h"
+#include "main.h"
 
-int timer0_counter = 0;
-int timer1_count = 0;
-int timer2_count = 0;
-int timer3_count =0;
+int timer1_count = 100;
+int timer2_count = 50;
+int status = 1;
 
-int timer0_flag = 0;
-int timer1_flag = 0;
-int timer2_flag = 0;
-int timer3_flag = 0;
-
-int TIMER_CYCLE = 10;
-
-void setTimer0 (int duration){
-	timer0_counter = duration  / TIMER_CYCLE;
-	timer0_flag = 0;
-}
-
-void setTimer1 (int duration){
-	timer1_count = duration  / TIMER_CYCLE;
-	timer1_flag = 0;
-}
-void setTimer2 (int duration){
-	timer2_count = duration  / TIMER_CYCLE;
-	timer2_flag = 0;
-}
-void setTimer3 (int duration){
-	timer3_count = duration  / TIMER_CYCLE;
-	timer3_flag = 0;
-}
 void timerRun(){
-	if (timer0_counter > 0){
-		timer0_counter--;
-		if (timer0_counter <= 0){
-			timer0_flag = 1;
-		}
-	}
+
 	if (timer1_count > 0){
 		timer1_count--;
 		if (timer1_count <= 0 ){
-			timer1_flag = 1;
+	  		  HAL_GPIO_TogglePin(GPIOA,LED_RED_Pin);
+	  		  HAL_GPIO_TogglePin(GPIOA,DOT_Pin);
+	  		  timer1_count = 100;
 		}
 	}
 	if (timer2_count > 0){
 		timer2_count--;
 		if (timer2_count <= 0){
-			timer2_flag = 1;
+			timer2_count= 50;
+	  		  switch (status){
+	  		  	  case 1:
+	  		  		status = 2;
+	  		  		HAL_GPIO_WritePin(GPIOA, EN0_Pin, GPIO_PIN_RESET);
+	  		  		HAL_GPIO_WritePin(GPIOA, EN2_Pin| EN3_Pin  | EN1_Pin, GPIO_PIN_SET);
+	  		  		display7SEG(1);
+	  		  		break;
+	  		  	  case 2:
+	  		  		HAL_GPIO_WritePin(GPIOA,  EN1_Pin, GPIO_PIN_RESET);
+	  		  		HAL_GPIO_WritePin(GPIOA, EN0_Pin | EN2_Pin | EN3_Pin, GPIO_PIN_SET);
+	  		  		status = 3;
+	  		  		display7SEG(2);
+	  		  		break;
+	  		  	  case 3:
+	  		  		HAL_GPIO_WritePin(GPIOA,EN2_Pin , GPIO_PIN_RESET);
+	  		  		HAL_GPIO_WritePin(GPIOA, EN0_Pin | EN1_Pin | EN3_Pin, GPIO_PIN_SET);
+	  		  		status = 4;
+	  		  		display7SEG(3);
+	  		  		break;
+	  		  	  case 4:
+	    		  	HAL_GPIO_WritePin(GPIOA, EN3_Pin, GPIO_PIN_RESET);
+	    		  	HAL_GPIO_WritePin(GPIOA, EN2_Pin | EN0_Pin | EN1_Pin, GPIO_PIN_SET);
+	  		  		status = 1;
+	  		  		display7SEG(0);
+	  		  		break;
+	  		  	  default:
+	  		  		display7SEG(0);
+	  		  		break;
+	  		  }
 		}
 	}
-	if (timer3_count > 0){
-		timer3_count--;
-		if (timer3_count <= 0){
-			timer3_flag = 1;
-		}
-	}
+
 }
 
 
